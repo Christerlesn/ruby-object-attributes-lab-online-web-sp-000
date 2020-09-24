57f28d856b5ef829a121ab30585aa10822212dd3
Christerle Nounoute <christerlesn@gmail.com>
2020-09-24 05:37:56 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+def initialize
+end
+
+def name=(dog_name)
+  @dog_name = dog_name
+end
+
+def name
+  @dog_name
+end
+
+def breed=(dog_type)
+  @dog_type = dog_type
+end
+
+def breed
+  @dog_type
+end
+
+
+end
